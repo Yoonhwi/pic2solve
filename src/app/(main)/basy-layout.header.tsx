114196fa51f0bd7e3844b444375590ef9e048d86
Yoonhwi <ush0105@naver.com>
2024-12-21 22:12:27 +0900

    feat: update text styling with font-family, font size, and localized theme switcher

--- src/app/(main)/basy-layout.header.tsx
+++ src/app/(main)/basy-layout.header.tsx
@@ -2,9 +2,9 @@ import { ThemeSwitcher } from "@/components/controls/theme-switcher";
 
 const BaseLayoutHeader = () => {
   return (
-    <div className="flex justify-center bg-secondary py-4 shadow-md">
+    <div className="flex justify-center h-[100px] shadow-md items-center">
       <div className="w-[1280px] flex justify-between">
-        <h1>Header</h1>
+        <h1 className="font-bold text-3xl">로고</h1>
         <ThemeSwitcher />
       </div>
     </div>
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import BaseLayout from "./base-layout";
 
 export default async function Page() {
   return (
     <BaseLayout>
-      <div>home</div>
+      <div className="flex flex-col gap-2">
+        <Button>Button</Button>
+        <Button>제출</Button>
+      </div>
     </BaseLayout>
   );
 }
