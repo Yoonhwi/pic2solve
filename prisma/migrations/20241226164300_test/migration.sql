8b6c31651bf223dba6bbe4b6f02dab284f44721e
Yoonhwi <ush0105@naver.com>
2024-12-27 01:47:32 +0900

    feat: add Prisma intergration with Supabase

---
+++ prisma/migrations/20241226164300_test/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profile" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nickname_key" ON "User"("nickname");
